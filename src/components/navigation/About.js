e2dddd2d8c0d25cc693a9609f39195424fcc76ae
David Wenzel <dwenzel95@gmail.com>
2022-09-10 08:32:00 -0500

    about component style

--- src/components/navigation/About.js
+++ src/components/navigation/About.js
@@ -1,10 +1,11 @@
 import Navbar from "./Navbar";
+import "./About.css"
 
 export default function About() {
   return (
-    <div>
+    <div className="about">
       <Navbar/>
-      <p> JOURNAL.me is both service and community, founded in the tradition of self exploration and reflection. We center outselves around well-being, design, art and self connection. When we take time to reflect we are able to connect to our values, intuitions, feelings, emotions, and our true selves.</p>
+      <p id="aboutP"> JOURNAL.me is both service and community, founded in the tradition of self exploration and reflection. We center outselves around well-being, design, art and self connection. When we take time to reflect we are able to connect to our values, intuitions, feelings, emotions, and our true selves.</p>
     </div>
   )
 }
