5da13acf591fc254bf16bbfe000cb31de2e2f734
David Wenzel <dwenzel95@gmail.com>
2022-09-10 13:18:21 -0500

    alt tag for logo added

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -15,7 +15,7 @@ const style = {
   return (
     <div>
       <NavLink
-      to="/homepage"><img id="navLogo" href="" src={'https://64.media.tumblr.com/0cbd98315d5588fb2e7b78eb002f1e54/8480379ca55bc022-78/s1280x1920/453f8a5fc6bfe78e8e7cd8d58b9f0384486fa027.pnj'}></img>
+      to="/homepage"><img id="navLogo" alt='logo'href="" src={'https://64.media.tumblr.com/0cbd98315d5588fb2e7b78eb002f1e54/8480379ca55bc022-78/s1280x1920/453f8a5fc6bfe78e8e7cd8d58b9f0384486fa027.pnj'}></img>
       </NavLink>
       {/* <NavLink 
       activeStyle={{fontWeight: "bold"}}
